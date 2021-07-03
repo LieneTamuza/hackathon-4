da7ee205346d04f480e2b6f9890a361ea195a546
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-03 10:25:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Header />
+                
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                    
+                </Switch>
+
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
