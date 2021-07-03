b7b84ef4026f6b516da3903446734bdb9a85a7f9
ievabi <ibidermane@gmail.com>
2021-07-03 11:44:54 +0200

    Added folder /Assets/css for css files, first version of carousel ( Slider.js) for Landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,12 @@
+import Slider from './Slider';
+
 function LandingPage() {
 
 
     return(
         <div className="container">
             <div className="row">
-                <h1>This is gonna be the landing page</h1>
+                <Slider />
             </div>
         </div>
     )
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,9 +1,50 @@
+import Slider1 from '../Assets/Images/Slider1.JPG';
+import Slider2 from '../Assets/Images/Slider2.JPG';
+import Slider3 from '../Assets/Images/Slider3.JPG';
+
 function Slider() {
 
-    return(
+    return (
 
         <div>
-            <h1>Here comes the slider</h1>
+            <div id="carouselCaptions" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={Slider1} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>High quality Landscape pcitures fro Nature Lovers</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={Slider2} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Marvelous close-ups of different flowrs for Nature Lovers</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                    <img src={Slider3} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Amazing pictures of cats for Cat Lovers</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
