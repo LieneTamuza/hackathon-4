4b7380644481a192479694cfb7e59ed9aeca595c
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-03 16:32:46 +0300

    Update Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,18 +1,18 @@
 import { SRLWrapper } from "simple-react-lightbox";
-import cat7 from '../Assets/Img/cats7.JPG';
-import cat1 from '../Assets/Img/cats1.JPG';
-import cat2 from '../Assets/Img/cats2.JPG';
-import cat3 from '../Assets/Img/cats3.JPG';
-import cat4 from '../Assets/Img/cats4.JPG';
-import cat5 from '../Assets/Img/cats5.JPG';
-import cat6 from '../Assets/Img/cats6.JPG';
+import cat7 from '../Assets/Images/cats7.JPG';
+import cat1 from '../Assets/Images/cats1.JPG';
+import cat2 from '../Assets/Images/cats2.JPG';
+import cat3 from '../Assets/Images/cats3.JPG';
+import cat4 from '../Assets/Images/cats4.JPG';
+import cat5 from '../Assets/Images/cats5.JPG';
+import cat6 from '../Assets/Images/cats6.JPG';
 
 function Products() {
 
     return (
         <div className="container mt-3">
             <h2 className="my-3">Products ...</h2>
-            <div className="row">
+            <div className="row mb-5">
                 <div className="col-12 col-md-5">
                     <SRLWrapper>
                         <div className="row">
@@ -50,7 +50,7 @@ function Products() {
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum quis in quam amet assumenda esse possimus eaque hic voluptatem dolores aperiam itaque reprehenderit rem blanditiis ex commodi, veritatis dolorem pariatur.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum quis in quam amet assumenda esse possimus eaque hic voluptatem dolores aperiam itaque reprehenderit rem blanditiis ex commodi, veritatis dolorem pariatur.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum quis in quam amet assumenda esse possimus eaque hic voluptatem dolores aperiam itaque reprehenderit rem blanditiis ex commodi, veritatis dolorem pariatur.</p>
-                    <div className="my-5"><strong>9.99 EUR</strong></div>
+                    <div className="my-3"><strong>9.99 EUR</strong></div>
                     <button className="btn btn-dark btn-lg" onClick={()=>{ alert('Added to cart!'); }}>Add to cart</button>
                 </div>
             </div>
