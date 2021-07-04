e08787171e3601c3824d64d89a358e3bd808772f
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-04 10:17:17 +0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import CartPage from './Views/CartPage';
-import CategoriesPage from './Views/CategoriesPage';
-import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import LandingPage from './Views/LandingPage';
-
+import CategoriesPage from './Views/CategoriesPage';
+import CategoryPage from './Views/CategoryPage';
+import ProductPage from './Views/ProductPage';
+import CartPage from './Views/CartPage';
 
 function App() {
     return (
         <div>
             <Router>
                 <Header />
+
                 <Switch>
                     <Route exact path='/'>
                         <LandingPage />
@@ -18,10 +20,17 @@ function App() {
                     <Route exact path='/categories'>
                         <CategoriesPage />
                     </Route>
+                    <Route exact path='/categories/category'>
+                        <CategoryPage />
+                    </Route>
+                    <Route exact path='/categories/category/products'>
+                        <ProductPage />
+                    </Route>
                     <Route exact path='/cart'>
                         <CartPage />
                     </Route>
                 </Switch>
+
                 <Footer />
             </Router>
         </div>
