eb8a04f3906ac074f602dfe0ce7cff5737eec188
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-08 20:57:28 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ function Header() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <NavLink exact to="/" className="nav-link">Home</NavLink>
