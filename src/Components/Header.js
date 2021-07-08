d0fd5df1727a42d938cece77144d485e538c506c
LieneTamuza <76061709+LieneTamuza@users.noreply.github.com>
2021-07-08 15:43:51 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,28 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 
 function Header() {
-    const [mainMenuCollapsed, setMainMenuCollapsed] = useState(true);
-
-    const toggleMainMenu = () => {
-        setMainMenuCollapsed(!mainMenuCollapsed);
-    }
-
-    let mainMenuClasses = 'collapse navbar-collapse';
-    if (!mainMenuCollapsed) {
-        mainMenuClasses += ' show';
-    }
-
-    let menuTogglerButtonClasses = 'navbar-toggler';
-    if (!mainMenuCollapsed) {
-        menuTogglerButtonClasses += ' collapsed';
-    }
-
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">LOGO</NavLink>
-                    <button onClick={toggleMainMenu} className={menuTogglerButtonClasses} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className={mainMenuClasses} id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <NavLink className="nav-link" exact to="/">Home</NavLink>
-                            <NavLink className="nav-link" to="/categories">Categories</NavLink>
-                            <NavLink className="nav-link" to="/cart">Cart</NavLink>
-                        </div>
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <NavLink to="/" className="navbar-brand">LOGO</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink exact to="/" className="nav-link">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/categories" className="nav-link">Categories</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/cart" className="nav-link">Cart</NavLink>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     )
 }
 
